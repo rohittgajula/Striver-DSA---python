9bb9adc52080de5db82ee324930584ac93ae24ed
rohit_gajula <rohitgajula27@gmail.com>
2022-11-15 17:16:37 +0530

    15-11-2022

--- Solve Problems on Array/repeatingElements.py
+++ Solve Problems on Array/repeatingElements.py
@@ -1,3 +1,4 @@
+# using while loop
 
 def repeatingElements(arr):
     n = len(arr)
@@ -10,6 +11,18 @@ def repeatingElements(arr):
         i += 1
     return temp
 
+# using for loop
+
+def RepeatingElements(arr):
+    n = len(arr)
+    temp = []
+
+    for i in range(n):
+        if arr[i] == arr[i-1]:
+            temp.append(arr[i])
+    return temp
+
+
 arr = [1,1,3,2,5,5,6,7,7]
 print(repeatingElements(arr))
-
+print(RepeatingElements(arr))
