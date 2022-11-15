77d28eda54ac90b06b0bcf83539b12764508a66b
rohit_gajula <rohitgajula27@gmail.com>
2022-11-15 17:43:09 +0530

    15-11-2022

---
+++ Solve Problems on Array/MaxConsecutiveOnes.py
@@ -0,0 +1,20 @@
+
+def MaxConsecutiveOnes(arr):
+    n = len(arr)
+    count = 0
+    result = 0
+
+    for i in range(n):
+        if (arr[i] == 0):
+            count = 0
+        else:
+            count += 1
+            print(result, end=' ')
+            result = max(result, count)
+    print()
+
+    return result
+
+arr = [1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0]
+print(MaxConsecutiveOnes(arr))
+
