64b8ae8f226c8147fa0c166b9cbbc27717643222
rohit_gajula <rohitgajula27@gmail.com>
2022-11-16 11:48:28 +0530

    16-11-2022

---
+++ Solve Problems on Array/SubarraySumEqualsK.py
@@ -0,0 +1,37 @@
+# method 1
+
+def SubarraySumEqualsK(arr, target):
+    n = len(arr)
+
+    if target in arr:
+        ans = [arr.index(target)]
+        return len(ans)
+    for i in range(n):
+        for j in range(n-1):
+            if (arr[i]+arr[j+1]) == target:
+                ans = [i,j+1]
+    return len(ans)
+
+# method 2
+
+def subarraySumEqualsK(arr, target):
+    pass
+
+    ans, n = 0, len(arr)
+    preSum = [arr[0]]
+    dic = {}
+    dic[0] = 1
+
+    for i in arr[1:]:
+        preSum.append(i+preSum[-1])
+    for i in preSum:
+        if i-target in dic:
+            ans += dic[i-target]
+        dic[i] = dic.get(i,0) + 1
+    return ans
+
+arr = [1,2,3]
+target = 2
+print(SubarraySumEqualsK(arr, target))
+print(subarraySumEqualsK(arr, target))
+
