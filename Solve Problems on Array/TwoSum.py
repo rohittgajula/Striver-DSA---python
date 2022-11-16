7cae111f2aa702111c2b930edf9f7c5d991272d7
rohit_gajula <rohitgajula27@gmail.com>
2022-11-16 11:13:08 +0530

    16-11-2022

--- Solve Problems on Array/TwoSum.py
+++ Solve Problems on Array/TwoSum.py
@@ -1,13 +1,16 @@
 def TwoSum(arr, target):
     n = len(arr)
 
-    for i in range(n):
-        for j in range(i,n-1):
-            if (arr[i] + arr[j+1]) == target:
-                return i,j+1
+    if target in arr:
+        return arr.index(target)
+    else:
+        for i in range(n):
+            for j in range(i,n-1):
+                if (arr[i] + arr[j+1]) == target:
+                    return i,j+1
 
 
 arr = [2,7,11,15]
-target = 17
+target = 9
 print(TwoSum(arr, target))
 
