3a3fb913f4affc741d7a26cf25b374792277a4af
rohit_gajula <rohitgajula27@gmail.com>
2022-11-18 11:29:40 +0530

    18-11-2022

--- Solve Problems on Array/Search2DMatrix.py
+++ Solve Problems on Array/Search2DMatrix.py
@@ -2,7 +2,7 @@ def SearchMatrix(matrix, target):
     rows = len(matrix)
     columns = len(matrix[0])
 
-    top = 0
+    top = 0             # looking for rows in this loop.
     bottom = rows - 1
     while top <= bottom:
         row = (top + bottom) // 2
@@ -13,12 +13,13 @@ def SearchMatrix(matrix, target):
         else:
             break
 
-        if not (top <= bottom):
-            return False
+    if not (top <= bottom):
+        return False
 
-    row = (top + bottom) // 2    # rows = 3
+           # 2 + 3 // 2  ==  2
+    row = (top + bottom) // 2
     l = 0
-    r = columns - 1              # columns = 4
+    r = columns - 1
     while l <= r:
         m = (l + r) // 2
         if target > matrix[row][m]:
@@ -29,7 +30,9 @@ def SearchMatrix(matrix, target):
             return True
     return False
 
-matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-target = 3
+matrix = [  [1,3,5,7],
+            [10,11,16,20],
+            [23,30,34,60]  ]
+target = 30
 print(SearchMatrix(matrix, target))
 
--- ex.py
+++ ex.py
@@ -1,10 +1,32 @@
+def first(arr, low, high):
 
-matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+    if high >= low:
+        mid = low + (high - low) // 2
+        if (mid == 0 or arr[mid-1] == 0 and arr[mid] == 1):
+            return mid
+        elif arr[mid] == 0:
+            return first(arr, (mid + 1), high)
+        else:
+            return first(arr, low, (mid - 1))
+    return -1
 
-m = len(matrix)
-n = len(matrix[0])
+def rowWithMin1s(mat):
 
-print(m, n)
+    r = len(mat)
+    c = len(mat[0])
+    max_row_index = 0
+    max = -1
+    for i in range(0, r):
+        index = first(mat[i], 0, c-1)
+        if index != -1 and c - index > max:
+            max = c - index
+            max_row_index = i
+    return max_row_index
 
-print(matrix[2][-1])
-
+mat  = [
+        [0,0,0,1],
+        [0,1,1,1],
+        [1,1,1,1],
+        [0,0,0,0]
+]
+print(rowWithMin1s(mat))
