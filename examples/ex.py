fc60e880403a3cece37f10c36ede396c2d5bed5b
rohit_gajula <rohitgajula27@gmail.com>
2023-02-11 09:30:34 +0530

    11-02-23

--- examples/ex.py
+++ examples/ex.py
@@ -1,7 +1,25 @@
 def BinarySearch(arr, target):
     n = len(arr)
+    arr.sort()
 
+    low = 0
+    high = n-1
+    Found = False
 
-arr = [1,4,3,2,5,7,8,0]
-target = 0
+    while low <= high and not Found:
+        mid = (low + high)//2
+        if (target < arr[mid]):
+            high = mid - 1
+        elif (target > arr[mid]):
+            low = mid + 1
+        else:
+            Found = True
+
+    if Found == True:
+        return "Target number is found."
+    else:
+        return "Target number is not found."
+
+arr = [3,5,1,6,2,8]
+target = 10
 print(BinarySearch(arr, target))
